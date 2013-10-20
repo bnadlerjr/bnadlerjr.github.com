674fe417216b8f5c7ba0d16fde78b611be8c93e1
Bob Nadler <bnadlerjr@gmail.com>
2013-10-20 09:03:09 -0400

    Initial commit.

---
+++ Gemfile
@@ -0,0 +1,5 @@
+source 'http://rubygems.org'
+
+gem "middleman",      "~> 3.1.6"
+gem "middleman-blog", "~> 3.3.0"
+gem "builder",        "~> 3.0"
---
+++ config.rb
@@ -0,0 +1,18 @@
+Time.zone = "US/Eastern"
+
+activate :blog do |blog|
+  blog.tag_template = "tag.html"
+  blog.calendar_template = "calendar.html"
+end
+
+page "/feed.xml", :layout => false
+
+set :css_dir, 'stylesheets'
+set :js_dir, 'javascripts'
+set :images_dir, 'images'
+
+configure :build do
+  activate :asset_hash
+  activate :minify_css
+  activate :minify_javascript
+end
