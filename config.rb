1f2e2923717354e9d924ad7379f28ad13878796c
Bob Nadler <bnadlerjr@gmail.com>
2021-12-13 19:03:34 -0500

    Cleanup some Rubocop warnings

--- config.rb
+++ config.rb
@@ -1,12 +1,14 @@
-Time.zone = "US/Eastern"
+# frozen_string_literal: true
+
+Time.zone = 'US/Eastern'
 
 helpers do
-  def full_url(url)
-    "http://bobnadler.com/#{url}"
+  def full_url(path, escape: false)
+    "http://bobnadler.com/#{path}"
   end
 
   def title
-    name = "Bob Nadler, Jr"
+    name = 'Bob Nadler, Jr'
     current_article ? "#{name} | #{current_article.title}" : name
   end
 
@@ -48,34 +50,34 @@ end
 activate :autoprefixer
 
 activate :blog do |blog|
-  blog.layout = "blog_layout"
+  blog.layout = 'blog_layout'
   blog.paginate = true
-  blog.prefix = "articles"
-  blog.new_article_template = "templates/new_article_template.markdown.erb"
+  blog.prefix = 'articles'
+  blog.new_article_template = 'templates/new_article_template.markdown.erb'
 end
 
 activate :deploy do |deploy|
   deploy.deploy_method = :git
   deploy.build_before = true
-  deploy.branch = "gh-pages"
+  deploy.branch = 'gh-pages'
 end
 
 activate :syntax
 set :markdown_engine, :redcarpet
 set :markdown, fenced_code_blocks: true
 
-page "/feed.xml", layout: false
-page "/sitemap.xml", layout: false
+page '/feed.xml', layout: false
+page '/sitemap.xml', layout: false
 
 set :css_dir, 'stylesheets'
 set :js_dir, 'javascripts'
 set :images_dir, 'images'
 
-set :url_root, "http://bobnadler.com"
+set :url_root, 'http://bobnadler.com'
 activate :search_engine_sitemap
 
 configure :build do
-  activate :asset_hash, ignore: "favicon.png"
+  activate :asset_hash, ignore: 'favicon.png'
   activate :minify_css
   activate :minify_javascript
 end
